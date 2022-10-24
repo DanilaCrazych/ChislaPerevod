91e206468cc9a4acbc5f5b6b56f57df891c69176
DanilaCrazych <Mag7istr@mail.ru>
2022-10-24 14:20:18 +0300

    new

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,18 @@
-package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /////////////////////
+        Perevod perevod = new Perevod();
+        perevod.Vosem(721);
+        perevod.Vosem(436);
+        perevod.Vosem(327);
+        perevod.Vosem(751);
+        perevod.Vosem(154);
+        perevod.Six("1D5");
+        perevod.Six("9EA");
+        perevod.Six("2CD");
+        perevod.Six("2D8");
+        perevod.Six("1D9");
+        ////////////////////
     }
 }
--- src/main/java/Perevod.java
+++ src/main/java/Perevod.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class Perevod {
+public class Perevod {
+    public void Vosem(long a){
+        long aber,out;
+        int raya;
+        int calhoon;
+
+        System.out.println("Исходное число: " + a);
+        calhoon = Integer.parseInt(String.valueOf(8));
+        aber = Long.parseLong(String.valueOf(a),calhoon);
+        raya = 2;
+        out = Long.parseLong(Long.toString(a,raya));
+        System.out.println("Двоичная система: " + out);
+        calhoon = Integer.parseInt(String.valueOf(2));
+        Long.parseLong(String.valueOf(out),calhoon);
+        raya = 10;
+        Long.parseLong(Long.toString(out,raya));
+        System.out.println("Десятиричная система: " + aber);
+        calhoon = Integer.parseInt(String.valueOf(10));
+        Long.parseLong(String.valueOf(aber),calhoon);
+        raya = 16;
+        System.out.println("Шестнацетиричная система: " + Long.toString(aber,raya));
+        System.out.println();
+
+    }
+
+    public void Six(String ab) {
+        System.out.println("Исходное число: " + ab);
+        long a = Long.valueOf(ab,16);
+        long out,out2,out3;
+        int raya;
+        int calhoon;
+        calhoon = Integer.parseInt(String.valueOf(10));
+        Long.parseLong(String.valueOf(a),calhoon);
+        raya = 2;
+        out = Long.parseLong(Long.toString(Long.parseLong(String.valueOf(a)),raya));
+        System.out.println("Двоичная система: " + out);
+        calhoon = Integer.parseInt(String.valueOf(2));
+        Long.parseLong(String.valueOf(out),calhoon);
+        raya = 8;
+        out2 = Long.parseLong(Long.toString(a,raya));
+        System.out.println("Восьмиричная система: " + out2);
+        calhoon = Integer.parseInt(String.valueOf(8));
+        out3 = Long.parseLong(String.valueOf(out2),calhoon);
+        raya = 10;
+        System.out.println("Десятиричная система: " + Long.toString(out3,raya));
+        System.out.println();
+    }
 }
+
+
+
